625ea5e2b52d5c72eb78bc2faca6bb5308b814a5
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-05-03 20:47:24 -0300

    Fix admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+
+    # Define admin model for custom User model with no email field.
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_trusty', 'is_staff',
+                                       'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('id', 'is_staff', 'email',)
+
+
+# @admin.register(Documents)
+# class DocumentsAdmin(admin.ModelAdmin):
+#     list_display = ['cpf', 'user', 'created_at']
+#     list_filter = ['cpf', 'user', 'created_at']
+#     search_fields = ['cpf', 'user']
--- api/settings.py
+++ api/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
